197bc450bd297fa3c1161bd82fd4ecc36148a4f8
waleed-cod <ranawlaee@gmail.com>
2021-03-27 14:23:41 +0500

    commit

--- app/src/main/java/com/codembeded/fleetfoot/activity/PickUp.java
+++ app/src/main/java/com/codembeded/fleetfoot/activity/PickUp.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.codembeded.fleetfoot.R;
 
-public class Login extends AppCompatActivity {
+public class PickUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_pick_up);
     }
 }
--- app/src/main/java/com/codembeded/fleetfoot/activity/SignUp.java
+++ app/src/main/java/com/codembeded/fleetfoot/activity/SignUp.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.codembeded.fleetfoot.R;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
     }
 }
