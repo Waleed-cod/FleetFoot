6c8f3a9a83357ca5cd7b6ff84414ca65dcea8754
waleed-cod <ranawlaee@gmail.com>
2021-04-09 15:50:00 +0500

    commit

--- app/src/main/java/com/codembeded/fleetfoot/activity/Login.java
+++ app/src/main/java/com/codembeded/fleetfoot/activity/Login.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,43 +35,52 @@ public class Login extends AppCompatActivity {
     TextInputEditText user_email_login_et, user_password_login_et;
     private static final String TAG = Login.class.getSimpleName();
     SharedPreferences sharedPreferences;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        login_btn = findViewById(R.id.logIn_btn);
-//        sign_up = findViewById(R.id.signUp_login_tv);
+        init();
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 deliveryBoyLogin(user_email_login_et.getText().toString(),user_password_login_et.getText().toString());
-                startActivity(new Intent(Login.this,Home.class));
             }
         });
     }
 
+    private void init() {
+
+        login_btn = findViewById(R.id.logIn_btn);
+        user_email_login_et = findViewById(R.id.user_email_login_et);
+        user_password_login_et = findViewById(R.id.user_password_login_et);
+        progressBar = findViewById(R.id.login_progress_bar);
+//        sign_up = findViewById(R.id.signUp_login_tv);
+    }
+
     private void deliveryBoyLogin(final String email_str, final String password_str) {
         String tag_str_req = "req_get_login";
-//        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
 
         StringRequest strReq = new StringRequest(Request.Method.POST, AppConfig.DELIVERY_BOY_LOGIN, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-//                progressBar.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
+                Log.e("1st response",response);
 
                 try {
                     JSONObject jObj = new JSONObject(response);
+                    Log.e("2nd response",response);
                     boolean error = jObj.getBoolean("error");
                     //check for error node in json
                     if (!error) {
                         sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString("id", jObj.getString("id"));
-                        editor.apply();
+//                        SharedPreferences.Editor editor = sharedPreferences.edit();
+//                        editor.putString("id", jObj.getString("id"));
+//                        editor.apply();
                         user_email_login_et.setText("");
                         user_password_login_et.setText("");
                         Intent intent = new Intent(Login.this, Home.class);
@@ -95,7 +105,7 @@ public class Login extends AppCompatActivity {
         }) {
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
-                params.put("phone_number", email_str);
+                params.put("email", email_str);
                 params.put("password", password_str);
 
                 return params;
